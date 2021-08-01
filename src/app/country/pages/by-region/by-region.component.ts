2b27d58a4c7f6c0cda2dcc14c9d820019b443597
David <davidsantander9@gmail.com>
2021-08-01 18:08:59 -0500

    show countries by capital

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -1,4 +1,7 @@
 import { Component} from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-region',
@@ -14,16 +17,30 @@ export class ByRegionComponent {
 
   regions: string[] = [ 'africa', 'americas', 'asia', 'europe', 'oceania'];
   activatedRegion: string = '';
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(private countryService: CountryService) { }
 
   getClassCss( region: string){
     return (region === this.activatedRegion ) ? 'btn btn-primary' : 'btn btn-outline-primary'
   }
 
   activateRegion( region: string){
+    if ( region === this.activatedRegion ) { return; }
     this.activatedRegion = region;
+    this.countries = [];
+    this.searchByRegion( region );
     //TODO: load a service
   }
+
+  searchByRegion( region: string){
+    this.countryService.getCountryByRegion( this.activatedRegion ).subscribe(
+        countries => {
+          this.countries = countries;
+        }, (err) => {
+          this.countries = [];
+        }
+      );
+    }
 
 }
